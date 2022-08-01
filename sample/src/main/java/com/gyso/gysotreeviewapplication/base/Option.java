f1a28b0e4bca75ae4a32587feea6eda510a80639
Valery Petrov <valeryvpetrov.dev@gmail.com>
2022-08-01 17:49:13 +0300

    Add options model

---
+++ sample/src/main/java/com/gyso/gysotreeviewapplication/base/Option.java
@@ -0,0 +1,9 @@
+package com.gyso.gysotreeviewapplication.base;
+
+public class Option {
+	private String name;
+
+	public Option(String name) {
+		this.name = name;
+	}
+}
--- sample/src/main/java/com/gyso/gysotreeviewapplication/base/Scene.java
+++ sample/src/main/java/com/gyso/gysotreeviewapplication/base/Scene.java
@@ -1,10 +1,14 @@
 package com.gyso.gysotreeviewapplication.base;
 
+import java.util.List;
+
 public class Scene {
     public String id;
+    public List<Option> options;
 
-    public Scene(String name) {
+    public Scene(String name, List<Option> options) {
         this.id = name;
+        this.options = options;
     }
 
     @Override
